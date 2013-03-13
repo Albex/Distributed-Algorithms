2f4d4581e8ce2e378f4a04818328de7b6d9cca2e
Alexandre Camus <alex.camus@ymail.com>
2013-03-13 14:01:06 +0000

    Start question 2.1.2

---
+++ src/EventuallyLeaderElector.java
@@ -0,0 +1,15 @@
+
+public class EventuallyLeaderElector extends EventuallyPerfectFailureDetector {
+
+	public EventuallyLeaderElector(Process p, int n) {
+		super(p, n);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getLeader() {
+		
+		return super.getLeader();
+	}
+
+}
